acf064e41f7120c74023ff6c6827f4d5657be4ff
Alex <olishevskyalex@gmail.com>
2021-03-12 23:01:29 +0300

    added component editor

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const serverConfig = require('./server-config.json');
+const mapConfig = require('./map-config.json');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send(index.html);
+  res.send('index.html');
+});
+
+app.get('/api', (req, res) => {
+  res.send(mapConfig);
 });
 
 app.listen(serverConfig.port, () => {
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './app.scss';
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  getOption(value, text) {
+    return <option key={value} value={value}>{text}</option>;
+  }
+  render() {
+    let options = [];
+    for (let i = 1; i <= 104; i++) {
+      options.push(this.getOption(i, `Номер участка: ${i}`));
+    }
+    return (
+      <form className="editor">
+        <select name="" id="">
+          {options}
+        </select>
+      </form>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="app">
-      123
+      <Editor />
     </div>
   );
 }
