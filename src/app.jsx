b0d910f37b43453fc74bf65242e9d2c56d4059ec
Alex <olishevskyalex@gmail.com>
2021-03-13 15:43:07 +0300

    installed module regenerator-runtime

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import regeneratorRuntime from "regenerator-runtime";
 import './app.scss';
 
 import Editor from './components/editor/editor.jsx';
 
-function App() {
-  return (
-    <div className="app">
-      <Editor />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getMapConfig();
+  }
+  async getMapConfig() {
+    let response = await fetch('/api');
+    console.log(response);
+  }
+  render() {
+    return (
+      <div className="app">
+        <Editor />
+      </div>
+    );
+  }
 }
 
 const root = document.querySelector('#root');
