11ea025ee9d901f189bbeeede90a14e3a87aa808
Alex <olishevskyalex@gmail.com>
2021-03-13 20:17:21 +0300

    component editor modified

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -2,26 +2,36 @@ import React from 'react';
 import s from './editor.module.scss';
 
 import EditorSelectGrounds from './editor__select-grouds.jsx';
+import EditorInput from './editor__input.jsx';
 
 export default class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
     this.state = {
-      number: '1',
-      size: '1',
-      price: '1',
+      number: '',
+      size: '',
+      price: '',
     }
     this.changeVeiwData = this.changeVeiwData.bind(this);
+    this.handleChangeInput = this.handleChangeInput.bind(this);
   }
   changeVeiwData(e) {
     const groundID = e.target.value;
     const groundActive = this.props.grounds[groundID];
-    this.state = {
+    this.setState({
       number: groundActive.number,
       size: groundActive.size,
       price: groundActive.price,
-    }
+    });
+    console.log(this.state);
+  }
+  handleChangeInput(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState({
+      [name]: value,
+    });
     console.log(this.state);
   }
   render() {
@@ -32,26 +42,23 @@ export default class Editor extends React.Component {
           grounds={this.props.grounds}
           onChange={this.changeVeiwData}
         />
-        <input 
-          name="number" 
-          className={s.input} 
-          type="text" 
+        <EditorInput
+          name="number"
           placeholder="Номер участка"
           value={this.state.number}
+          onChange={this.handleChangeInput}
         />
-        <input 
+        <EditorInput 
           name="size" 
-          className={s.input} 
-          type="text" 
           placeholder="Размер" 
           value={this.state.size}
+          onChange={this.handleChangeInput}
         />
-        <input 
-          name="price" 
-          className={s.input} 
-          type="text" 
+        <EditorInput 
+          name="price"
           placeholder="Цена"
           value={this.state.price}
+          onChange={this.handleChangeInput}
         />
         <button className={s.button} type="submit">Cохранить</button>
       </form>
